0b830608648f1464a14f6c8f011c817b680fb49e
jaeheon jung <>
2021-05-17 20:02:05 +0900

    메인 화면 수정사항 적용

--- src/main/java/io/cojam/web/dao/QuestDao.java
+++ src/main/java/io/cojam/web/dao/QuestDao.java
@@ -1,9 +1,6 @@
 package io.cojam.web.dao;
 
-import io.cojam.web.domain.Betting;
-import io.cojam.web.domain.MyVoting;
-import io.cojam.web.domain.Quest;
-import io.cojam.web.domain.QuestAnswer;
+import io.cojam.web.domain.*;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
@@ -54,4 +51,6 @@ public interface QuestDao {
     Betting getBettingDetail(Betting betting);
 
     int updateBettingSuccess(Betting betting);
+
+    List<Question> getHomeQuestList(Question question);
 }
